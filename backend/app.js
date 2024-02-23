153d40ab64bcc20394c2c9968fa6a87809749157
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-02-23 16:01:45 +0100

    added base db connection

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose  = require ('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const MONGO_URI = 'mongodb://localhost:27017/gamestore'; // Replace with your MongoDB connection string
 
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
