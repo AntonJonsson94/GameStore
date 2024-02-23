3864fe82b0b083c0e9ca208001eec88e10c621e4
Anton Jonsson <gmodderable@gmail.com>
2024-02-23 16:27:00 +0100

    added db connection and some schemas

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb://localhost/gamestore';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
 
 app.use(logger('dev'));
 app.use(express.json());
