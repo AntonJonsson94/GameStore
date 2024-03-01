222042a15ee3febc7b4f9e3ca71a17979eed20ba
Anton Jonsson <gmodderable@gmail.com>
2024-03-01 08:13:07 +0100

    added theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,6 +15,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
+    gamerTheme: {
+      "primary": "#00C0B4",
+      "secondary": "#D10382",
+      "accent": "#32D1C7",
+      "neutral": "#172144",
+      "base-100": "#1742144",
+      "info": "#f3f4f6",
+      "success": "#84cc16",
+      "warning": "#d97706",
+      "error": "#e11d48",
+    },
 };
 export default config;
