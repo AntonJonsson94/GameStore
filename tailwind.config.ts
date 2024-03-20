5e8cb397bbadc8eae6487164af3a02547c531d58
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-03-20 08:09:20 +0100

    added daisy ui & theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,19 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
+  plugins: [require("daisyui")],
+  gamerTheme: {
+      primary: "#00C0B4",
+      secondary: "#D10382",
+      accent: "#32D1C7",
+      neutral: "#172144",
+      "base-100": "#1742144",
+      info: "#f3f4f6",
+      success: "#84cc16",
+      warning: "#d97706",
+      error: "#e11d48",
     },
-  },
-  plugins: [],
-};
+  }
+  
 export default config;
