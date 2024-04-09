81d02025384133d407d806aec7c005d6323a3961
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-04-09 10:17:10 +0200

    update paths
    Co-authored-by: Anton Jonsson <AntonJonsson94@users.noreply.github.com>

--- src/app/api/deals/route.ts
+++ src/app/api/deals/route.ts
@@ -1,14 +1,18 @@
 import { IGame, ICheapSharkGame } from "@/models/interfaces";
 import { Game } from "@/models/schemas";
+import {
+  cheapSharkFiveFreeGames,
+  cheapSharkFiveDeals
+} from "@/services/apiRequests";
 import { createGame } from "@/services/createGames";
-import { cheapSharkFiveFreeGames, cheapSharkFiveDeals } from "@/utils/apiRequests";
-const dbConnect = require('@/lib/dbConnect')
+
+const dbConnect = require("@/lib/dbConnect");
 
 export async function GET() {
   try {
     await dbConnect();
 
-    const games:any[] = [];
+    const games: any[] = [];
 
     const freeGames = await cheapSharkFiveFreeGames();
 
@@ -19,40 +23,35 @@ export async function GET() {
     });
 
     const highestDealRatedGames = await cheapSharkFiveDeals();
-    highestDealRatedGames.forEach((game:any) => {
+    highestDealRatedGames.forEach((game: any) => {
       if (game.dealRating === "10.0") {
         games.push(game);
       }
     });
 
-    const gamesToDisplay:IGame[] = [] 
+    const gamesToDisplay: IGame[] = [];
     //get unique games from the combined data, with the free ones taking prio
-    const fiveCheapSharkGames:ICheapSharkGame[] = Array.from(new Set(games.map((game:any) => game.title)))
-      .map((title) => games.find((game:any) => game.title === title))
+    const fiveCheapSharkGames: ICheapSharkGame[] = Array.from(
+      new Set(games.map((game: any) => game.title))
+    )
+      .map((title) => games.find((game: any) => game.title === title))
       .slice(0, 5);
 
+    for (const game of fiveCheapSharkGames) {
+      const existingGame = await Game.findOne({
+        cheap_shark_id: game.gameID
+      });
+      // console.log(existingGame)
+      createGame(game);
+      if (!existingGame) {
+        gamesToDisplay.push(await createGame(game));
+      } else {
+      }
+    }
 
-
-      for (const game of fiveCheapSharkGames) {
-        const existingGame = await Game.findOne({
-          cheap_shark_id: game.gameID
-        });
-        // console.log(existingGame)
-        createGame(game)
-        if(!existingGame) {
-          gamesToDisplay.push(await createGame(game))
-        } else {
-
-        }
-     }
-
-
-
-    return Response.json(gamesToDisplay)
-
-
+    return Response.json(gamesToDisplay);
   } catch (error) {
-    console.log(error)
-    return new Response("error")
+    console.log(error);
+    return new Response("error");
   }
 }
