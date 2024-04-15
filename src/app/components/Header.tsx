f72f83057b96c7259dccccf958e41dec2881da60
Anton Jonsson <anton.jonsson@ericsson.com>
2024-04-15 15:26:45 +0200

    header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <h1 className="logo-value">Value</h1>
         <h1 className="logo-town">Town</h1>
       </div>
-      <div className=" divider divider-info justify-center"></div>
+      <div className="divider divider-info justify-center"></div>
       <h1 className="text-xl" text-xl>
         The Best Deals in Gaming!
       </h1>
