86bb2c6504ba329c962923782fa69d715d4b2a5e
Anton Jonsson <anton.jonsson@ericsson.com>
2024-05-20 07:54:13 +0200

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    experimental: {
-        serverComponentsExternalPackages: ["mongoose"],
-    },
+   reactStrictMode: false,
+   experimental: {
+      serverComponentsExternalPackages: ["mongoose"],
+   },
+   images: {
+       remotePatterns: [
+           {
+               protocol: 'https',
+               hostname: 'media.rawg.io',
+           },
+           {
+               protocol: 'https',
+               hostname: 'images.igdb.com',
+           },
+           {
+               protocol: 'https',
+               hostname: 'cdn.cloudflare.steamstatic.com',
+           },
+       ],
+   },
 };
 
 export default nextConfig;
