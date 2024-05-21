6a2e3ba91b2e93cda5558b27bd373739f6a3c68c
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-05-21 14:15:44 +0200

    removed log DB connected

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -5,7 +5,6 @@ const dbConnect = async () => {
     await mongoose.connect(`${process.env.MONGODB_URI}`, {
       serverSelectionTimeoutMS: 5000,
     });
-    console.log("Database connected successfully");
   } catch (error) {
     console.error("Error connecting to the database:", error);
     process.exit(1);
