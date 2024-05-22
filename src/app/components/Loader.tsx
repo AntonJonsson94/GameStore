a5b9e5bd13caf84a5f970a2914c937d07f0d9d53
Anton Jonsson <anton.jonsson@ericsson.com>
2024-05-22 11:11:37 +0200

    create a loader component

---
+++ src/app/components/Loader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div className="flex flex-wrap gap-6 w-auto h-auto justify-center">
+      <span className="loading loading-ring loading-xs"></span>
+      <span className="loading loading-ring loading-sm"></span>
+      <span className="loading loading-ring loading-md"></span>
+      <span className="loading loading-ring loading-lg"></span>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
 "use client";
 import useGames from "@/hooks/useGames";
 import { IGame } from "@/models/interfaces";
-import Image from "next/image";
 import Card from "./components/Card";
+import Loader from "./components/Loader";
 
 export default function FrontPage() {
   const { games, gamesLoading } = useGames();
 
   return (
     <section className="flex flex-wrap place-items-center p-10 h-auto">
-      {gamesLoading ?? (
-        <div className="flex flex-wrap gap-6 w-auto h-auto justify-center">
-          <span className="loading loading-ring loading-xs"></span>
-          <span className="loading loading-ring loading-sm"></span>
-          <span className="loading loading-ring loading-md"></span>
-          <span className="loading loading-ring loading-lg"></span>
-        </div>
-      )}
+      {gamesLoading ?? <Loader />}
       <div className="grid grid-flow-rows gap-4 grid-cols-1 md:grid-cols-3 m-auto h-auto justify-center">
         {games &&
           games.map((game: IGame, index: number) => (
