1a27713b4b0ecbe6b87ac89af0be185dd0bdf9be
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-05-28 13:13:53 +0200

    hide seachbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import useGames from "@/hooks/useGames";
 import { IGame } from "@/models/interfaces";
 import Loader from "./components/Loader";
 import Card from "./components/Card";
-import Searchbar from "./components/Searchbar";
 
 export default function FrontPage() {
   const { games, gamesLoading } = useGames();
 
   return (
     <section className="flex flex-wrap place-items-center p-10 h-auto">
-      <Searchbar />
       {gamesLoading ?? <Loader />}
       <div className="grid grid-flow-rows gap-4 grid-cols-1 md:grid-cols-3 m-auto h-auto justify-center">
         {games &&
