0c84441c98f6a2c64818b4854ab86586e016d697
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-05-30 22:32:11 +0200

    added game info and description

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Divider from "@/app/components/Divider"
 import { IGame } from "@/models/interfaces"
 import Image from "next/image"
+import Link from "next/link"
 
 export default async function GamePage({ params }: { params: { id: string } }) {
 
@@ -12,8 +14,27 @@ export default async function GamePage({ params }: { params: { id: string } }) {
   const game: IGame = await getGame()
 
   return (
-    <main className="container flex justify-center mx-auto">
-      <h1>{game.title}</h1>
-    </main>
+    <>
+      <section id="game-info" className="flex flex-column justify-between">
+        <h1 className="text-6xl text-info w-1/2">{game.title}</h1>
+        <div>
+          <h1 className="text-info text-5xl">BEST PRICE</h1>
+          <span className="stroke-current text-info line-through">{game.full_price}</span> - <span className="bg-red-500 text-info">{Math.round(Number(game.discount))}%</span>
+          <h3 className="font-bold text-info text-3xl">{game.lowest_price}€ </h3>
+          <Link target="_blank" href={game.cheapest_link}><button className="btn btn-primary w-full">BUY NOW</button></Link>
+        </div>
+      </section>
+
+      <Divider />
+
+      <article className="text-info" id="overview">
+        <h1 className="text-3xl">Overview</h1>
+        <p>{game.description}</p>
+      </article>
+
+      <Divider />
+
+
+    </>
   )
 }
