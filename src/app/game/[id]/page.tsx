4888e74bca4f3c4db26d17a5e81574d7e9b31943
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-05-31 21:18:13 +0200

    align single page

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,25 +1,28 @@
 import Divider from "@/app/components/Divider";
 import StoreCard from "@/app/components/StoreCard";
-import { IGame, IStore, IStoreOffer } from "@/models/interfaces";
-import { StoreOffer } from "@/models/schemas";
+import { IGame } from "@/models/interfaces";
 import Link from "next/link";
 
-export default async function GamePage({ params }: { params: { id: string } }) {
-  async function getGame() {
-    const res = await fetch(`http://localhost:3000/api/game/${params.id}`);
-    if (!res.ok) return;
-    return res.json();
-  }
 
-  const game: IGame = await getGame();
+async function getGame(id: string) {
+  const res = await fetch(`http://localhost:3000/api/game/${id}`);
+  if (!res.ok) return;
+  return res.json();
+}
+
+export default async function GamePage({ params }: { params: { id: string } }) {
+
+
+  const game: IGame = await getGame(params.id);
+
   const offers = game.store_offers.map((offer) => (
     <StoreCard key={offer.dealID} offer={offer} />
   ));
   return (
     <>
-      <section id="game-info" className="flex flex-column justify-between">
+      <section id="game-info" className="flex mt-6 flex-column justify-between">
         <h1 className="text-6xl text-info w-1/2">{game.title}</h1>
-        <div>
+        <p>
           <h1 className="text-info text-5xl">BEST PRICE</h1>
           <span className="stroke-current text-info line-through">
             {game.full_price}
@@ -34,19 +37,20 @@ export default async function GamePage({ params }: { params: { id: string } }) {
           <Link target="_blank" href={game.cheapest_link}>
             <button className="btn btn-primary w-full">BUY NOW</button>
           </Link>
-        </div>
+        </p>
       </section>
 
       <Divider />
 
       <article className="text-info" id="overview">
-        <h1 className="text-3xl">Overview</h1>
+        <h1 className="text-5xl">Overview</h1>
         <p>{game.description}</p>
       </article>
 
       <Divider />
 
-      <div className="grid gap-16 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+      <h1 className="text-5xl text-info">MORE DEALS</h1>
+      <div className="mt-6 grid gap-16 grid-cols-1 md:grid-cols-3">
         {offers}
       </div>
     </>
