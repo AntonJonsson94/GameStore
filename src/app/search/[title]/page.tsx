e9335ba9c559c009150c4fe8408780d2e087c42b
Anton Jonsson <94antjon@skola.boras.se>
2024-06-05 07:55:18 +0200

    wip search result page

--- src/app/search/[title]/page.tsx
+++ src/app/search/[title]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import useSearchGames from "@/hooks/useSearchGames";
 import { IGame } from "@/models/interfaces";
 import Card from "@/app/components/Card";
@@ -10,17 +10,27 @@ type Params = {
 
 export default function SearchResults({ params }: { params: Params }) {
   const { title } = params;
-  const { games } = useSearchGames(title);
 
+  const [games, setGames] = useState<IGame[]>()
+  const getGames = async (): Promise<Array<IGame>> => await fetch(`http://localhost:3000/api/search/${title}`).then(data => data.json())
+  getGames().then(games => setGames(games))
+  // const { games } = useSearchGames(title);
+// const mockeddata = [{
+
+
+// }]
  
   return (
-    <section className="flex flex-wrap place-items-center p-10 h-auto">
+    <>
+    <p>hej</p>
+    {/* <section className="flex flex-wrap place-items-center p-10 h-auto">
       <div className="grid grid-flow-rows gap-4 grid-cols-1 md:grid-cols-3 m-auto h-auto justify-center">
         {games &&
           games.map((game: IGame, index: number) => (
             <Card game={game} key={index} />
           ))}
       </div>
-    </section>
+    </section> */}
+    </>
   );
 }
