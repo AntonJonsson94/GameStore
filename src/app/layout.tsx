ee985345a9d469e8d2e10683aad0bfa5dabeae89
Anton Jonsson <94antjon@skola.boras.se>
2024-06-05 14:09:17 +0200

    removed searchbar from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Searchbar from "./components/Searchbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +15,6 @@ export default function RootLayout({
     <html data-theme="gamerTheme" lang="en">
       <body className={`${inter.className}`}>
         <Header />
-        <Searchbar/>
         <main className="w-2/3 mx-auto">{children}</main>
         <Footer />
       </body>
