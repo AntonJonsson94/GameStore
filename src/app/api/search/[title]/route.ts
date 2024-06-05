5d3a45401377d6d9df685592f5033ead95011729
Anton Jonsson <94antjon@skola.boras.se>
2024-06-05 11:56:35 +0200

    used different method

--- src/app/api/search/[title]/route.ts
+++ src/app/api/search/[title]/route.ts
@@ -1,41 +1,44 @@
 import { ICheapSharkGame, IGame } from "@/models/interfaces";
-
 import updateOrCreateGame from "@/services/updateOrCreateGame";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { title: string } }
-) {
+export async function GET(request: Request, { params }: { params: { title: string } }) {
     try {
         const cheapsharkgameResponse: ICheapSharkGame[] = await fetch(
-          `https://www.cheapshark.com/api/1.0/deals?title=${params.title}&pageSize=20`,
-          { next: { revalidate: 86400 } }
+            `https://www.cheapshark.com/api/1.0/deals?title=${params.title}&pageSize=20`,
+            { next: { revalidate: 86400 } }
         ).then((res) => res.json());
-    
-        const uniqueGamesMap = new Map<string, ICheapSharkGame>();
-    
+
+     
+        const uniqueGameIds = new Set<string>();
+     
+        const filteredGames: ICheapSharkGame[] = [];
         for (const cheapSharkGame of cheapsharkgameResponse) {
-          const existingGame = uniqueGamesMap.get(cheapSharkGame.gameID);
-          if (!existingGame || cheapSharkGame.salePrice < existingGame.salePrice) {
-            uniqueGamesMap.set(cheapSharkGame.gameID, cheapSharkGame);
-          }
-        }
-        
-        const gamesToDisplay: IGame[] = [];
-        for (const game of uniqueGamesMap.values()) {
-          try {
-            const gameToPush = await updateOrCreateGame(game);
-            if (gameToPush) {
-              gamesToDisplay.push(gameToPush);
+            if (!uniqueGameIds.has(cheapSharkGame.gameID)) {
+                uniqueGameIds.add(cheapSharkGame.gameID);
+                filteredGames.push(cheapSharkGame);
+            } else {
+                const existingGameIndex = filteredGames.findIndex(game => game.gameID === cheapSharkGame.gameID);
+                if (existingGameIndex !== -1 && cheapSharkGame.salePrice < filteredGames[existingGameIndex].salePrice) {
+                    filteredGames[existingGameIndex] = cheapSharkGame;
+                }
             }
-          } catch (error) {
-            console.error("Error inserting to DB:", error);
-          }
         }
-    
+
+        const gamesToDisplay: IGame[] = [];
+        for (const cheapSharkGame of filteredGames) {
+            try {
+                const gameToPush = await updateOrCreateGame(cheapSharkGame);
+                if (gameToPush) {
+                    gamesToDisplay.push(gameToPush);
+                }
+            } catch (error) {
+                console.error("Error inserting to DB:", error);
+            }
+        }
+
         return Response.json(gamesToDisplay);
-      } catch (error) {
+    } catch (error) {
         console.error(error);
         return Response.error();
-      }
+    }
 }
